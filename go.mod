7f62eb0ad4927d43b5bf27cd4750fe22919a8f11
mahardhika21 <dboyscoz@gmail.com>
2023-04-19 03:29:34 +0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mahardhika21/golang-test.git
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/golang-test/helper"
+)
+
+func main() {
+	fmt.Println(hello_world.HelloWorld("zul"))
+}
